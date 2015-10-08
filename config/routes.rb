14f4a09d8cc51f9664ec8fd7a8fc01bd1ce22aa7
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-10-07 21:06:50 -0400

    Added static index, contact, and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
