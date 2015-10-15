7d904dedbcec6fb686e0f2f3cc3decba4e9a10bf
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-10-14 23:36:55 -0400

    fixed issue with price format

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015030432) do
+ActiveRecord::Schema.define(version: 20151015033442) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "colour"
-    t.decimal  "price"
+    t.decimal  "price",       precision: 8, scale: 2
   end
 
 end
