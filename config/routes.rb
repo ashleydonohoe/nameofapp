20728dfb18a834849395fa9373412f3c1778c984
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-10-16 21:50:11 -0400

    added orders controller, views and set the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :new, :create]
+
   get 'static_pages/coming_soon'
 
   get 'static_pages/about'
@@ -12,7 +14,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
