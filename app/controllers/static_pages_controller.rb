174efb0386b565ed776f1539d64490420ebf691d
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-10-28 23:20:55 -0400

    added email form on contact page and set up to use Sendgrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,15 @@ class StaticPagesController < ApplicationController
   def landing_page
   	@products = Product.limit(3)
   end
+
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@phone = params[:phone]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(:from => @email,
+  		:to => 'ashleyrdonohoe@gmail.com',
+  		:subject => "A new contact form message from #{@name}",
+  		:body => @message).deliver_now
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  post 'static_pages/thank_you'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
