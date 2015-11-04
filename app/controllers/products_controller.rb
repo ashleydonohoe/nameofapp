f799dd2389f3804fb4b492a8e092bd5cbaf26d5c
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-11-03 21:43:47 -0500

    changed authentication for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /products
   # GET /products.json
