d150a65c06230391ecc2b5f71babd08a2a582d49
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-12-10 00:42:29 -0500

    trying account confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 	has_many :orders
-	after_create :send_welcome
-
-	def send_welcome
-		UserNotifier.send_signup_email(self).deliver
-	end
 end
