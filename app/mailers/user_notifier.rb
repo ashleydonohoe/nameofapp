51e7bedf106f4bd49ef9056ff7757a49c2072456
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-12-10 00:14:24 -0500

    fixing the email at signup

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -3,6 +3,7 @@ class UserNotifier < ApplicationMailer
 
 	def send_signup_email(user)
 		@user = user
-		mail( :to => @user.email, :subject => "Thanks for signing up!").deliver_now
+		mail( :to => @user.email, 
+			:subject => 'Thanks for signing up!')
 	end
 end
