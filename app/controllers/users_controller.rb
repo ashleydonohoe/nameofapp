8ed5bd7e37623439f482a71e750f72a6937581fa
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-12-10 00:28:29 -0500

    changed mailer file name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        UserNotifier.send_signup_email(@user).deliver
+        UserNotifier.send_signup_email(@user).deliver_now
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
