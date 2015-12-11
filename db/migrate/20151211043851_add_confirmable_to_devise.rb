89284014a69651460414aa3fa0753539a4b577b9
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-12-10 23:43:50 -0500

    Trying to fix database migration

--- db/migrate/20151211043851_add_confirmable_to_devise.rb
+++ db/migrate/20151211043851_add_confirmable_to_devise.rb
@@ -1,7 +1,6 @@
 class AddConfirmableToDevise < ActiveRecord::Migration
   # Note: You can't use change, as User.update_all will fail in the down migration
   def up
-    add_column :users, :confirmation_token, :string
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
     add_index :users, :confirmation_token, unique: true
