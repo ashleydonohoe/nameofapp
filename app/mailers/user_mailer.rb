129760ee16edb40997e33424ae6ca09b98f90dae
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-12-11 00:12:56 -0500

    added code for welcome message email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,11 @@ class UserMailer < ApplicationMailer
 			:to => 'ashleyrdonohoe@gmail.com',
 			:subject => "A new contact form message from #{name} at #{phone}")
 	end
+
+	def welcome_email(user)
+		@user = user
+		@url = 'http://gadget-mart.herokuapp.com/'
+		mail(to: @user.email, subject: 'Welcome to Gadget Mart!')
+	end
+
 end
