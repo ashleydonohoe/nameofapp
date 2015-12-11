24190eca080d394b833d5e9b4fce7cc4b204290f
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-12-10 22:22:23 -0500

    preparing to test confirmable from devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210065801) do
+ActiveRecord::Schema.define(version: 20151208043413) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -60,12 +60,8 @@ ActiveRecord::Schema.define(version: 20151210065801) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.boolean  "admin",                  default: false, null: false
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
