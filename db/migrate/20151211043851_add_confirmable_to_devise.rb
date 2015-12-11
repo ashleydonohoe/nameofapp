f44da2f50de209b1a707120e94b954951243d95b
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-12-10 23:44:56 -0500

    testing again

--- db/migrate/20151211043851_add_confirmable_to_devise.rb
+++ db/migrate/20151211043851_add_confirmable_to_devise.rb
@@ -4,7 +4,6 @@ class AddConfirmableToDevise < ActiveRecord::Migration
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
     add_index :users, :confirmation_token, unique: true
-	User.all.update_all confirmed_at: Time.now
   end
 
   def down
