aca29956f46feff21c96077a9ae230201d9cc93f
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-01-09 16:50:22 -0500

    changed production logger level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,8 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  Rails.logger.level = 2
+
+  config.log_level = :warn
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
